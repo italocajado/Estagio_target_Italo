f4eee4ec8482e08722618b02cea20cea078f8dac
italocajado <italocajado@gmail.com>
2025-01-12 15:46:52 -0300

    ATT

--- q3Vetor.py
+++ q3Vetor.py
@@ -13,7 +13,3 @@ print(f"O valor mínimo de faturamento é: {minFat}")
 print(f"O valor máximo de faturamento é: {maxFat}")
 print(f"O valor médio de faturamento é: {medFat}")
 print(f"Há {diaAcimaMed} dias com faturamento acima da média")
-
-#exemplo de json
-# {"dia": 1, "valor": 22174.1664},
-# {"dia": 2, "valor": 247853.1664},
